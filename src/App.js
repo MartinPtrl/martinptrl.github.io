18fa9bde005a3381e98f5d08b1efa58e32bc80a9
MartinPtrl <martin.petrla@gmail.com>
2019-07-01 22:29:30 +0200

    Create react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>martin petrla</h1>
+        <h4>software engineer @microsoft | microsoft teams | calling & meetings</h4>
+      </header>
+      <body>
+        welcome to my personal website. the site is supposed to never be finished!
+      </body>
+    </div>
+  );
+}
+
+export default App;
