c46a4be235898effd8db9a28010df73cda320fc6
MartinPtrl <martin.petrla@gmail.com>
2019-07-01 23:15:25 +0200

    Updates

---
+++ precache-manifest.aacfe490fbf51ff1f5f46e22625f2ea1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d538cefe6b0bc7033521ecd4859fe8af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "291fc9bdcf43b45fa403",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "291fc9bdcf43b45fa403",
+    "url": "/static/js/main.63d8845b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
